b025ebf2cfc824e7d8d15347c5a1fb9e72ac50e1
eyesick <59859373+eyesick@users.noreply.github.com>
2020-07-28 02:19:05 -1000

    Add files via upload

---
+++ tictactoe/js/tictactoe.js
@@ -0,0 +1,39 @@
+let activePlayer = "X";
+
+let selectedSquares = [];
+
+function placeXOrO(squareNumber) {
+    if (!selectedSquares.some(element => element.includes(squareNumber))) {
+        let select = document.getElementById("squareNumber");
+        if (activePlayer === "X") {
+            select.style.backgroundImage = 'url("/images/x.png")';
+        } else {
+        select.style.backgroundImage = 'url("images/o.png")';
+        }
+        selectedSquares.push(squareNumber + activePlayer);
+        checkWinConditions();
+        if (activePlayer === "X") {
+            activePlayer = "O";
+        } else {
+            activePlayer = "X";
+        }
+        Audio('./media/place.mp3');
+        if (activePlayer === "O"){
+            disableClick();
+            setTimeout(function (){computersTurn(); }, 1000);
+        }
+        return true;
+    }
+
+    function computersTurn() {
+        let success = false;
+        let pickASquare;
+        while (!success){
+            pickASquare = String(Math.floor(Math.random() * 9));
+            if(placeXOrO(pickASquare)){
+                placeXOrO(pickASquare)
+                success = true;
+            };
+        }
+    }
+}
